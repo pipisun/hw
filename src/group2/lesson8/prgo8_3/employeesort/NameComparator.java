80890680a60d9f3ce20b93fb02ec54cb06f821a6
YongGY <715guoyong@gmail.com>
2016-12-13 17:10:17 -0600

    .

--- src/group2/lesson8/prgo8_3/employeesort/NameComparator.java
+++ src/group2/lesson8/prgo8_3/employeesort/NameComparator.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 public class NameComparator implements Comparator<Employee> {
 
+	
 	public int compare(Employee e1, Employee e2) {
 		String name1 = e1.getName();
 		String name2 = e2.getName();
